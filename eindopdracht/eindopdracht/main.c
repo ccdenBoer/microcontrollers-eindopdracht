be795dfdaad5a4f969497efc006e90bd40b007cf
Coen <cc.denboer1@student.avans.nl>
2023-03-22 10:22:09 +0100

    initial project

---
+++ eindopdracht/eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * eindopdracht.c
+ *
+ * Created: 22-3-2023 10:15:39
+ * Author : Coen den Boer & Momin Musa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
