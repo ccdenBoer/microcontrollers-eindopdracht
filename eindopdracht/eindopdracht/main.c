ac26f5d76a06f1cbd2c3aa7fb0e9519b478c1776
Coen <cc.denboer1@student.avans.nl>
2023-03-22 10:35:38 +0100

    added lcd support

--- eindopdracht/eindopdracht/main.c
+++ eindopdracht/eindopdracht/main.c
@@ -5,11 +5,25 @@
  * Author : Coen den Boer & Momin Musa
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
-
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lcd/lcd.h"
 
 int main(void)
 {
+	DDRC = 0xFF;
+	PORTC = 0x00;
+	
+	init_4bits_mode();
+	lcd_clear();
+	lcd_write_string("yoo");
+	
     /* Replace with your application code */
     while (1) 
     {
